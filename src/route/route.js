55c02303b4cca0c7c50881d9abc4a8c6038ceab2
AkhileshPatil12168 <akhileshpatil12168@gmail.com>
2022-10-09 19:31:48 +0530

    final

--- src/route/route.js
+++ src/route/route.js
@@ -6,8 +6,6 @@ const {createUrl, getUrl} = require("../controller/urlController")
 router.post ("/url/shorten", createUrl)
 router.get('/:urlCode',getUrl)
 
-
 router.all("/*", (req, res) => { res.status(400).send({ status: false, message: "Endpoint is not correct plese provide a proper end-point" }) })
-
 
 module.exports=router
